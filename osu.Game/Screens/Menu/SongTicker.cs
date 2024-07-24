0aa5c56fdd604a47279f6756793acd30118ea001
Walavouchey <36758269+Walavouchey@users.noreply.github.com>
2024-07-24 10:38:23 +0200

    disable some main menu ui components

--- osu.Game/Screens/Menu/SongTicker.cs
+++ osu.Game/Screens/Menu/SongTicker.cs
@@ -29,81 +29,15 @@ namespace osu.Game.Screens.Menu
 
         public SongTicker()
         {
-            AutoSizeAxes = Axes.Both;
-            InternalChildren = new Drawable[]
-            {
-                new Container
-                {
-                    Anchor = Anchor.TopRight,
-                    Origin = Anchor.TopRight,
-                    Position = new Vector2(5, -5),
-                    Padding = new MarginPadding(-5),
-                    RelativeSizeAxes = Axes.Both,
-                    Children = new Drawable[]
-                    {
-                        new CircularContainer
-                        {
-                            RelativeSizeAxes = Axes.Both,
-                            Masking = true,
-                            EdgeEffect = new EdgeEffectParameters
-                            {
-                                Radius = 75,
-                                Type = EdgeEffectType.Shadow,
-                                Colour = OsuColour.Gray(0.04f).Opacity(0.3f),
-                            },
-                            Children = new Drawable[]
-                            {
-                                new Box
-                                {
-                                    RelativeSizeAxes = Axes.Both,
-                                    AlwaysPresent = true,
-                                    Alpha = 0,
-                                },
-                            }
-                        },
-                    }
-                },
-                new FillFlowContainer
-                {
-                    AutoSizeAxes = Axes.Both,
-                    Direction = FillDirection.Vertical,
-                    Spacing = new Vector2(0, 3),
-                    Children = new Drawable[]
-                    {
-                        title = new OsuSpriteText
-                        {
-                            Anchor = Anchor.TopRight,
-                            Origin = Anchor.TopRight,
-                            Font = OsuFont.GetFont(size: 24, weight: FontWeight.Light, italics: true)
-                        },
-                        artist = new OsuSpriteText
-                        {
-                            Anchor = Anchor.TopRight,
-                            Origin = Anchor.TopRight,
-                            Font = OsuFont.GetFont(size: 16)
-                        }
-                    }
-                },
-            };
         }
 
         protected override void LoadComplete()
         {
             base.LoadComplete();
-
-            beatmap.BindValueChanged(_ => Scheduler.AddOnce(show), true);
         }
 
         private void show()
         {
-            var metadata = beatmap.Value.Metadata;
-
-            title.Text = new RomanisableString(metadata.TitleUnicode, metadata.Title);
-            artist.Text = new RomanisableString(metadata.ArtistUnicode, metadata.Artist);
-
-            this.FadeInFromZero(fade_duration / 2f)
-                .Delay(4000)
-                .Then().FadeOut(fade_duration);
         }
     }
 }
